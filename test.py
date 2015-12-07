e25508526742f46bc050f17ca18ef770cadfa2ac
CaterinaM <caterinamasotti@yahoo.it>
2015-12-07 18:33:39 +0100

    aggiunta start rule set alla grammatica

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ from CYK import *
 G=Grammar('S')
 G.add_rules_from_file('gramm_test')
 #print G.get_unit_productions()
-s='b a b b'
+s='she eats a fish with a fork'
 parser=CYK(G,s)
 print parser.parse()
