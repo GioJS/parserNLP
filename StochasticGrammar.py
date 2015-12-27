905f57c6cef827e584cd11e1a7ef74a8441a5cb9
giordanocristini <giordano.cristini@gmail.com>
2015-12-27 14:27:23 +0100

    finalmente finito

--- StochasticGrammar.py
+++ StochasticGrammar.py
@@ -8,23 +8,25 @@ class StochasticGrammar(Grammar):
 
 	def init_chances(self):
 		for group in self.groups:
-			chances=StochasticDict()
+			chances={}
 			if len(self.groups[group])==1:
 				chances[self.groups[group][0]]=1.0
 				self.grammar_chances[group]=chances
 				#print self.grammar[self.groups[group][0]], 1.0
 			else:
-
+				rands=[random.uniform(0,1) for i in range(len(self.groups[group]))]
+				s = sum(rands)
+				rands = [ i/s for i in rands ]
 				for rule in self.groups[group]:
 					#while True:
 					#	try:
-					chances[rule]=random.uniform(0,1)
+					chances[rule]=rands.pop()
 							#break
 					#	except Exception as e:
 					#		pass
 					#print self.grammar[rule], chances[rule]
-				print chances
-				chances.normalize()
+				
+				#chances.normalize()
 				self.grammar_chances[group]=chances
 
 
