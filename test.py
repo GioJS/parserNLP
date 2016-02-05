501c78df072260612025a6eee1aa16f72960ff82
giordanocristini <giordano.cristini@gmail.com>
2016-02-05 14:47:51 +0100

    si ricomincia

--- test.py
+++ test.py
@@ -1,19 +1,31 @@
+#encoding: utf-8
 from CYK import *
-G=Grammar('S')
-G.add_rules_from_file('gramm_test')
-#print G.get_unit_productions()
-s='she eats a fish with a fork'
-parser=CYK(G,s)
-parser.parse()
-#print parser.D
-#print parser.derivations()
-#pre-t
-#print parser.D
 
-print parser.getTrees()
-# from StochasticGrammar import *
+def true_answer(ans):
+    if ans.strip().lower() in ['true','1','yes','ok','sì','si','ja','da','oui']:
+        return True
+    return False
 
-# G=StochasticGrammar('S')
-# G.add_rules_from_file('BigGrammar')
-# G.init_chances()
-# print G.grammar_chances
+prob=true_answer(raw_input('probabilistico? '))
+
+
+if not prob:
+	G=Grammar('S')
+	G.add_rules_from_file('gramm_test')
+	#print G.get_unit_productions()
+	s='she eats a fish with a fork'
+	parser=CYK(G,s)
+	parser.parse()
+	#print parser.D
+	#print parser.derivations()
+	#pre-t
+	#print parser.D
+
+	print parser.getTrees()
+else:
+	from StochasticGrammar import *
+
+	G=StochasticGrammar('S')
+	G.add_rules_from_file('BigGrammar')
+	G.init_chances()
+	print G.grammar_chances
