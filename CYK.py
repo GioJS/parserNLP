43ea4cb10b6abac0f9b3798e3d7a1c5a803b0942
giordanocristini <giordano.cristini@gmail.com>
2016-02-10 10:51:03 +0100

    clear

--- CYK.py
+++ CYK.py
@@ -54,22 +54,7 @@ class ChartMatrix:
                 si+=str(self[i,j]) if self[i,j] else '\t-\t'
             s+=si+'\n'
         return s
-# class Node:
-#     def __init__(self,rule,parent,l_child,r_child):
-#         self.rule=rule
-#         self.parent=parent
-#         self.l_child=l_child
-#         self.r_child=r_child
-#         '''
-#         flag di visita
-#         '''
-#         self.flag=False
-#     def __repr__(self):
-#         return str(self.rule)+' => '+(str(self.l_child.rule) if self.l_child else '')+' & '+(str(self.r_child.rule) if self.r_child else '')
-#     def checked(self):
-#         return self.flag
-#     def check(self):
-#         self.flag=True
+
 class CYK:
     
     def __init__(self,G,s):
@@ -85,16 +70,6 @@ class CYK:
         #self.D=[]
         #definisce la struttura chart
         self.C=ChartMatrix(self.n)
-        #print self.C.matrix
-        '''
-        crea matrice 3D di nodi
-        '''
-        # for i in range(self.n):
-        #     self.D.append(list())
-        #     for j in range(self.n):
-        #         self.D[i].append(list())
-        #         for k in range(self.r):
-        #             self.D[i][j].append(Node(None,None,None,None))
 
     def parse(self):
         '''
@@ -106,8 +81,7 @@ class CYK:
                     #inizializza il primo livello
                     #print rule
                     self.C[i,i].addChart(rule,0)
-                    # self.D[0][i][rule.index].rule=rule
-                    # self.D[0][i][rule.index].check()
+                    
         #print "non terminals [ok]"
         #for i=1 to n -> i=1 to n+1
         for i in range(1,self.n):
@@ -126,54 +100,19 @@ class CYK:
                         # print rule_B, rule_C
                         for b in rule_B:
                             for c in rule_C:
-                                #print i
-                                #print k,i-k
-                                #print j+k+1,self.n
-                                #print self.D[k][j][b].checked(),self.D[i-k][j+k+1][c].checked()
-                                # if  self.D[k][j][b].checked() and self.D[i-k][j+k+1][c].checked():
-                                #     '''
-                                #     se k,j,b e i-k,j+k,c sono stati visitati dal CYK
-                                #     allora li aggiunge come figli di i,j,rule.index
-                                #     e si marca come visitato
-                                #     '''
-                                #     self.D[i+1][j][rule.index].l_child=self.D[k][j][b]
-                                #     self.D[i+1][j][rule.index].r_child=self.D[i-k][j+k+1][c]
-                                #     self.D[i+1][j][rule.index].rule=rule
-                                #     self.D[i+1][j][rule.index].check()
-                                #     '''
-                                #     assegna ai figli il padre
-                                #     '''
-                                #     self.D[k][j][b].parent=rule
-                                #     self.D[i-k][j+k+1][c].parent=rule
-                                #print j,k,self.C[j,j+k]
                                 if self.G[b] in self.C[j,j+k] and self.G[c] in self.C[j+k+1,j+i]:
                                     self.C[j,i+j].addChart(rule,j+k+1)
     
     def getTrees(self):
         '''
-        quello che mi e venuto in mente e abbastanza strambo:
-        ricreo la situazione del metodo parse:
-        e vado a a controllare quello che viene messo negli indici di cui sopra
-        poi viene una cosa che forse non ha senso ma nemmeno troppo sbagliata
-        se r1 e r2 fanno riferimento a r(r->r1 r2) vedo se il valore:
-        r1.split_point+r.split_point=r2.split_point+r.split_point
-        che forse mi deve dire che i due sono lo split del primo (r)
-        questo funziona quasi del tutto ma solo in 3 casi e vero
-        mancano altri casi, forse mi sto avvicinando XD
+        Ottenere gli alberi per mezzo di BOH
+        BOH
+        BOH
+        BOH
+        BOH
         '''
         trees=[]
-        for i in range(1,self.n):
-            for j in range(0,self.n-i):
-                for k in range(0,i):
-                    for r1 in self.C[j,j+k]:
-                        i_r1=r1.split_point
-                        for r2 in self.C[j+k+1,j+i]:
-                            i_r2=r2.split_point
-                            for r in self.C[j,i+j]:
-                                #print r1,r2,r
-                                print i_r1,i_r2,r.split_point
-                                if (r.rule[0]==r1.rule.head() and r.rule[1]==r2.rule.head()) and i_r1+r.split_point==i_r2+r.split_point:
-                                    print r,r1,r2
+        
 
 
         
